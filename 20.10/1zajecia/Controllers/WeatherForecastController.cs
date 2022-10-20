e6a8d94a0fddd8d929966d6a98c4eb242fa6bd57
Mateusz "Mattu" Partyka <mateusz.pl671@gmail.com>
2022-10-20 09:20:10 +0200

    add 20.10

---
+++ 20.10/1zajecia/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using _1zajecia.Services;
+
+namespace _1zajecia.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")] //mapowanie nazwy 
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var Calculate = new Calculate(24);
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet]
+        public int GetValue()
+        {
+            var calculateInstance = new Calculate(24);
+            calculateInstance.Increase(10);
+            calculateInstance.Decrease(23);
+            return calculateInstance.GetValue();
+        }
+
+        [HttpGet]
+        public List<int> GetValueList()
+        {
+            var calculateInstance = new Calculate(24);
+            var calculateInstance2 = new Calculate(50);
+            return new List<int>() { calculateInstance2.GetValue(), calculateInstance.GetValue() };
+        }
+    }
+}
---
+++ 20.10/1zajecia/Services/Calculate.cs
@@ -0,0 +1,24 @@
+﻿namespace _1zajecia.Services
+{
+    public class Calculate
+    {
+        public Calculate(int initValue) //constructor
+        {
+            Value = initValue;
+        }
+        private int Value { get; set; }
+        public void Increase(int value)
+        {
+            this.Value += value;
+        }
+        public void Decrease(int value)
+        {
+            this.Value -= value;
+        }
+        public int GetValue()
+        {
+            return Value;
+        }
+
+    }
+}
